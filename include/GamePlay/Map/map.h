d64d6f77adc74d91f8c22179bd7dc50a34981e88
ThanhTien102 <tienngo06092006@gmail.com>
2025-11-17 11:30:22 +0700

    feat/BackGround-Move

--- include/GamePlay/Map/map.h
+++ include/GamePlay/Map/map.h
@@ -4,11 +4,13 @@
 #include <memory>
 #include <vector>
 #include <iostream>
+#include "GamePlay/Physics/PhysicsSystem.h"
 
 class Map {
 private:
     sf::Texture backgroundTexture;
   public:
-    void map1(sf::RenderWindow &window, sf::Font &menuFont, sf::Sprite &backgroundSprite,
-              sf::Sprite &sunSprite, sf::Sprite &treeSprite, sf::RectangleShape &ground, sf::RectangleShape &ground2);
+    void map1(sf::RenderWindow &window, sf::Font &menuFont, sf::Sprite &backgroundSprite, sf::Sprite &backgroundSprite2,
+              sf::Sprite &sunSprite, sf::Sprite &treeSprite, sf::RectangleShape &ground, sf::RectangleShape &ground2,
+              std::vector<Obstacle> &obstacles);
 };
